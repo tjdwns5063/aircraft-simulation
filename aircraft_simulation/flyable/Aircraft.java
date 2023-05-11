25acee3a82890553d7e8d23b1192947fa44c7c8e
Seongjun Kim <seongjki@c8r3s2.42seoul.kr>
2023-05-11 19:35:48 +0900

    Implement Aircraft class.

---
+++ aircraft_simulation/flyable/Aircraft.java
@@ -0,0 +1,13 @@
+package aircraft_simulation.flyable;
+
+public class Aircraft {
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+
+    protected Aircraft(long p_id, String p_name, Coordinates p_coordinates) {
+        id = p_id;
+        name = p_name;
+        coordinates = p_coordinates;
+    }
+}
