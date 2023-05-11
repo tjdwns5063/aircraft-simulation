4a66dbcc5e1fb04ed72b208b07bc01a8bdf09aac
Seongjun Kim <seongjki@c8r3s2.42seoul.kr>
2023-05-11 19:40:08 +0900

    Implement Coordinates.
    - Implement Coordinated class simply.

---
+++ aircraft_simulation/flyable/Coordinates.java
@@ -0,0 +1,25 @@
+package aircraft_simulation.flyable;
+
+public class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    protected Coordinates(int p_longitude, int p_latitude, int p_height) {
+        longitude = p_longitude;
+        latitude = p_latitude;
+        height = p_height;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
