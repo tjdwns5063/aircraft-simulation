b3525a86c7904b6ca80feed2dbd6562903ab1101
seongjunkim <tjdwns5063@naver.com>
2023-05-12 13:26:17 +0900

    Implement Helicopter.
    - Implement empty Helicopter class.

---
+++ aircraft_simulation/flyable/Helicopter.java
@@ -0,0 +1,11 @@
+package aircraft_simulator.flyable;
+
+public class Helicopter extends Aircraft {
+    public Helicopter(long p_id, String p_name, Coordinates p_coordinates) {
+        super Aircraft(p_id, p_name, p_coordinates);
+    }
+
+    public void updateCondition() {
+        
+    }
+}
