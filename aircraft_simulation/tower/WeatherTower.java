9e886e905ed96b99b3db2ec3706e3c615e0ccd9c
seongjunkim <tjdwns5063@naver.com>
2023-05-13 16:52:53 +0900

    Implement WeatherTower.getWeather(Coordinates) method.
    - implement empty getWeather method.

--- aircraft_simulation/tower/WeatherTower.java
+++ aircraft_simulation/tower/WeatherTower.java
@@ -1,8 +1,11 @@
 package aircraft_simulation.tower;
 
+import aircraft_simulation.simulator.WeatherProvider;
+import aircraft_simulation.flyable.Coordinates;
+
 public class WeatherTower extends Tower {
-    public String getWeather() {
-        return "SNOW";
+    public String getWeather(Coordinates p_coordinates) {
+        return WeatherProvider.create().getCurrentWeather(p_coordinates);
     }
 
     public void changeWeather() {
